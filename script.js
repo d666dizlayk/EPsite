8232b85f0f5592305913c8b6f1d9a57baea51588
Stas <130368840+d666dizlayk@users.noreply.github.com>
2025-05-03 18:30:22 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+﻿// Easy Programming - базовая логика
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Easy Programming запущен');
+    // В дальнейшем здесь будет логика для форм, модальных окон и т.п.
+});
